30d43c63cef52db1d1c5635591713dc503f8bcc2
Leo Pitsillides <leo.pitsillides@gmail.com>
2022-11-20 19:43:05 +0000

    Adds in most conditionals for a pawn to move to a new position. Everything is collected into one method called move.

--- Pieces/pawn.py
+++ Pieces/pawn.py
@@ -34,14 +34,65 @@ class Pawn(Pieces):
     def is_en_passant():
         pass
 
+    ## Dont know if this is neccessary, will see after next method is written
+    def is_valid_move(self, new_pos):
+        if (Pawn.is_take_move(new_pos), Pawn.is_reg_move(new_pos), Pawn.is_start_move(new_pos)) == (False, False, False):
+            return False
+        else:
+            return True
 
-    def is_valid_move(self, new_pos, board):
 
-        if board[new_pos[0]][new_pos[1]] == None:
-            continue
-        elif:
+    '''
+    Just trying to get an idea of how 'board' will be structured.
+    Think the best idea would for it to be an 8x8 matrix.
+    Each position will contain the piece or None.
+    '''
 
-        
-        if self.start_pos == self.current_pos:
-            # can move two spaces
+    def move(self, new_pos, board):
+
+        if Pawn.is_reg_move(new_pos) == True:
+            # if position is not empty give a invalid message
+            if board[new_pos[0]][new_pos[1]].colour != None:
+                print("This is an invalid move. A piece already exists in this position.")
+            else:
+                #sets previous position to None
+                board[self.current_pos[0]][self.current_pos[1]] == None
+                #changes current position
+                self.current_pos == new_pos
+        elif Pawn.is_take_move(new_pos) == True:
+            # if position is empty either we cant move or it is an en passant
+            if board[new_pos[0]][new_pos[1]].colour == None:
+                ## en passant condition
+                pass
+            # if our piece is in new_pos then this is an invalid move
+            elif board[new_pos[0]][new_pos[1]].colour == self.colour:
+                print("This is an invalid move. One of your pieces already exists in this position.")
+            else:
+                #sets previous position to None
+                board[self.current_pos[0]][self.current_pos[1]] == None
+                #sets oppositions piece to dead
+                board[new_pos[0]][new_pos[1]].is_alive == False
+                #changes current position
+                self.current_pos == new_pos
+        elif Pawn.is_start_move(new_pos) == True:
+            '''
+            ##need to add a bit which check whether all positions in between current_pos and new_pos are empty
+            ##will probably need this in every piece except for knights so good to put into method under Pieces class
+            pos_dir =  new_pos - self.current_pos
+            check_pos = self.current_pos
+            for i in range(1, 1+distance):
+                check_pos += i*pos_dir
+                if board[check_pos[0]][check_pos[1]].colour != None:
+                    print("This is an invalid move")
+                    break
+            '''
+            if board[new_pos[0]][new_pos[1]].colour != None:
+                print("This is an invalid move. A piece already exists in this position.")
+            else:
+                #sets previous position to None
+                board[self.current_pos[0]][self.current_pos[1]] == None
+                #changes current position
+                self.current_pos == new_pos
+        else:
+            print("This is an invalid move. Please try again.")
 
