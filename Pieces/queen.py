a5acb303abe5f418676cedcdf783030a1db22dfb
Leo Pitsillides <leo.pitsillides@gmail.com>
2022-11-23 17:04:56 +0000

    Adds comments to chess.py file

--- Pieces/queen.py
+++ Pieces/queen.py
@@ -9,18 +9,25 @@ class Queen(Pieces):
 
     def is_move(self, new_pos):
         pos_dir = new_pos - self.current_pos
+        #first conditional covers diagonal movement
+        #second covers vertical/horizontal movement
         if abs(pos_dir[0]) == abs(pos_dir[1]) or np.all(pos_dir != 0) == False:
             return True
         else:
             return False
 
     def move(self, new_pos, board):
+        #check if new_pos is a valid move
         if self.is_move(new_pos) == True:
+            #check if any pieces exist between current_pos and new_pos
             if self.simple_check_pos(new_pos, board) == True:
+                # if nothing exists in new_pos then move there
                 if board[new_pos[0]][new_pos[1]] == None:
                     self.apply_move(new_pos, board)
+                # if own colour exists in new_pos then invalid move
                 elif board[new_pos[0]][new_pos[1]].colour == self.colour:
                     print("This is an invalid move. One of your pieces already exists in this position.")
+                # if opposite colour exists in new_pos then take piece
                 else:
                     self.apply_take(new_pos, board)
             else:
@@ -37,8 +44,15 @@ def test1():
     queen_white.move(np.array([5,2]), Pieces.board)
     print(Pieces.board)
 
-#Piece in the way
+#Regular horizontal move
 def test2():
+    queen_white = Queen('white', np.array([7,4]))
+    print(Pieces.board)
+    queen_white.move(np.array([7,2]), Pieces.board)
+    print(Pieces.board)
+
+#Piece in the way
+def test3():
     queen_white = Queen('white', np.array([7,4]))
     queen_black = Queen('black', np.array([6,3]))
     print(Pieces.board)
@@ -46,14 +60,14 @@ def test2():
     print(Pieces.board)
 
 #Taking a piece
-def test3():
+def test4():
     queen_white = Queen('white', np.array([7,4]))
     queen_black = Queen('black', np.array([5,2]))
     print(Pieces.board)
     queen_white.move(np.array([5,2]), Pieces.board)
     print(Pieces.board)
 
-test3()
+test2()
 
 
 
